fe29f81ea91797a884311a349d426bd2e15f2ed6
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-06 23:26:51 +0530

    Added:logic for part time

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,15 +1,20 @@
 public class EmployeeWages {
     public static void main(String[] args) {
-        int ispresent = 1;
+        int fulltime = 1;
+        int parttime = 0;
         int rateperhour = 20;
+        int emphrs = 0;
         int mysalary = 0;
-        int hours = 8;
         System.out.println("Welcome to the Employee Wages");
         double empcheck = Math.floor(Math.random() * 10) % 2;
-        if (ispresent == empcheck)
-            mysalary = (rateperhour * hours);
+        if (fulltime == empcheck)
+            emphrs = 8;
+        else if (parttime == empcheck)
+            emphrs = 4;
+        mysalary = (rateperhour * emphrs);
         System.out.println("salary per day:" +mysalary);
     }
 }
+
 
 
