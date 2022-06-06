eb509f78fa3776f468f56238b89e013cc6e73f5e
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-06 23:35:41 +0530

    Added:calculating salary per day using switch case

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,18 +1,28 @@
 public class EmployeeWages {
     public static void main(String[] args) {
-        int fulltime = 1;
-        int parttime = 0;
-        int rateperhour = 20;
-        int emphrs = 0;
-        int mysalary = 0;
+        int rateperhrs= 20;
+        int fulltime= 1;
+        int parttime= 2;
+        int empwage= 0;
+        int emphr = 0;
+        int empcheck = (int)Math.floor(Math.random()*10)%3;
         System.out.println("Welcome to the Employee Wages");
-        double empcheck = Math.floor(Math.random() * 10) % 2;
-        if (fulltime == empcheck)
-            emphrs = 8;
-        else if (parttime == empcheck)
-            emphrs = 4;
-        mysalary = (rateperhour * emphrs);
-        System.out.println("salary per day:" +mysalary);
+        switch (empcheck)
+        {
+            case 1:
+                emphr = 4;
+                break;
+
+            case 2:
+                emphr = 8;
+                break;
+
+            default:
+                emphr =0;
+                break;
+        }
+        empwage =  ( emphr * rateperhrs );
+        System.out.println ("salary per day" +empwage );
     }
 }
 
