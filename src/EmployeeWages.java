baf63589f644dad92e8f435aea199320c4d960b0
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-06 23:17:04 +0530

    Added:logic for calculcating salary per day

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,14 +1,14 @@
 public class EmployeeWages {
     public static void main(String[] args) {
         int ispresent = 1;
-
+        int rateperhour = 20;
+        int mysalary = 0;
+        int hours = 8;
         System.out.println("Welcome to the Employee Wages");
         double empcheck = Math.floor(Math.random() * 10) % 2;
-        if (empcheck == ispresent)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
-
+        if (ispresent == empcheck)
+            mysalary = (rateperhour * hours);
+        System.out.println("salary per day:" +mysalary);
     }
 }
 
