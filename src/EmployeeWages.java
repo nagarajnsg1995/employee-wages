1178898d7d1d06038313f34f01a4642646a83e00
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-06 23:06:13 +0530

    Added:logic for employee attendance

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,5 +1,15 @@
 public class EmployeeWages {
     public static void main(String[] args) {
+        int ispresent = 1;
+
         System.out.println("Welcome to the Employee Wages");
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+        if (empcheck == ispresent)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
+
     }
 }
+
+
