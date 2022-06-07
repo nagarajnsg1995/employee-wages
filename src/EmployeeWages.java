6f460ad65524cb1af66803646fbc0a8eb3a45641
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-07 11:10:03 +0530

    Added:monthly salary

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,30 +1,38 @@
 public class EmployeeWages {
     public static void main(String[] args) {
-        int rateperhrs= 20;
-        int fulltime= 1;
-        int parttime= 2;
-        int empwage= 0;
+        int Rateperhrs = 20;
+        int Fulltime = 1;
+        int Parttime = 2;
+        int empwage = 0;
         int emphr = 0;
-        int empcheck = (int)Math.floor(Math.random()*10)%3;
+        int workdays = 20;
+        int totalWage = 0;
+
         System.out.println("Welcome to the Employee Wages");
-        switch (empcheck)
-        {
-            case 1:
-                emphr = 4;
-                break;
+        for (int days = 0; days <= workdays; days++) {
+            int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 
-            case 2:
-                emphr = 8;
-                break;
+            switch (empcheck) {
+                case 1:
+                    emphr = 4;
+                    break;
 
-            default:
-                emphr =0;
-                break;
+                case 2:
+                    emphr = 8;
+                    break;
+
+                default:
+                    emphr = 0;
+                    break;
+            }
+            empwage = (emphr * Rateperhrs);
+            totalWage = empwage  ;
+            System.out.println("salary per day:" + empwage);
         }
-        empwage =  ( emphr * rateperhrs );
-        System.out.println ("salary per day" +empwage );
+        System.out.println("salary per month :" + totalWage  );
+
+
     }
 }
-
 
 
