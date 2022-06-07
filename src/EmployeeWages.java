d3fcca7d5b7be023ab1620a380ff92ffdc376a6a
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-07 12:05:55 +0530

    Added:multipel companies employee wages

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -1,22 +1,12 @@
 public class EmployeeWages {
-    public static final int emprateperhrs = 20;
-    public static final int empfulltime = 1;
-    public static final int empparttime = 2;
-    public static final int empmaxhrs = 100;
-    public static final int empnoworkingdays = 20;
-
-
-    public static void employeeWage() {
+    public static void hero(int emprateperhrs, int empnoworkingdays, int empmaxhrs) {
         int emphr = 0;
         int totalemphrs = 0;
         int emptotalworkingdays = 0;
         int empwage = 0;
-        System.out.println("Welcome to the Employee Wages");
         while (totalemphrs <= empmaxhrs && emptotalworkingdays < empnoworkingdays) {
             emptotalworkingdays++;
             int empcheck = (int) Math.floor(Math.random() * 10) % 3;
-
-
             switch (empcheck) {
                 case 1:
                     emphr = 4;
@@ -30,16 +20,84 @@ public class EmployeeWages {
                     emphr = 0;
             }
             totalemphrs += emphr;
-            System.out.println(emptotalworkingdays + "emphrs" + emphr);
+
         }
         empwage = (emphr * emprateperhrs);
-        System.out.println("per day emp wage for " + emphr + "is:" + empwage);
+        System.out.println("Hero company wage:");
+        System.out.println("per day employee wage for " + emphr + "is:" + empwage);
         int hrs = totalemphrs * emprateperhrs;
         System.out.println("total wage for " + totalemphrs + "hrs is :" + hrs);
+    }
+
+
+    static void honda(int emprateperhrs, int empnoworkingdays, int empmaxhrs) {
+        int emphr = 0;
+        int totalemphrs = 0;
+        int emptotalworkingdays = 0;
+        int empwage = 0;
+        while (totalemphrs <= empmaxhrs && emptotalworkingdays < empnoworkingdays) {
+            emptotalworkingdays++;
+            int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empcheck) {
+                case 1:
+                    emphr = 4;
+                    break;
+
+                case 2:
+                    emphr = 8;
+                    break;
+
+                default:
+                    emphr = 0;
+            }
+            totalemphrs += emphr;
+
+        }
+        empwage = (emphr * emprateperhrs);
+        System.out.println("Honda company wage:");
+        System.out.println("per day employee wage for " + emphr + "is:" + empwage);
+        int hrs = totalemphrs * emprateperhrs;
+        System.out.println("total wage for " + totalemphrs + "hrs is :" + hrs);
+    }
+
+
+    static void suzuki(int emprateperhrs, int empnoworkingdays, int empmaxhrs) {
+        int emphr = 0;
+        int totalemphrs = 0;
+        int emptotalworkingdays = 0;
+        int empwage = 0;
+        while (totalemphrs <= empmaxhrs && emptotalworkingdays < empnoworkingdays) {
+            emptotalworkingdays++;
+            int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empcheck) {
+                case 1:
+                    emphr = 4;
+                    break;
+
+                case 2:
+                    emphr = 8;
+                    break;
+
+                default:
+                    emphr = 0;
+            }
+            totalemphrs += emphr;
+        }
+        empwage = (emphr * emprateperhrs);
+        System.out.println("Suzuki company wage:");
+        System.out.println("per day employee wage for " + emphr + "is:" + empwage);
+        int hrs = totalemphrs * emprateperhrs;
+        System.out.println("total wage for " + totalemphrs + "hrs is :" + hrs);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Employee Wages");
+        EmployeeWages wage = new EmployeeWages();
+        wage.hero(20, 20, 100);
+        wage.honda(30, 25, 120);
+        wage.suzuki(40, 30, 150);
 
     }
-    public static void main(String[] args) {
-        employeeWage();
-    }
 }
+
 
